9a48fbb1144313b31bafa2fd6b509ec144b6ff78
mattdean <matthewallandean@gmail.com>
2021-02-15 20:29:29 -0700

    Initial Commit

---
+++ blog-swift/blog_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  blog_swiftApp.swift
+//  blog-swift
+//
+//  Created by Matthew Dean on 2/15/21.
+//
+
+import SwiftUI
+
+@main
+struct blog_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
